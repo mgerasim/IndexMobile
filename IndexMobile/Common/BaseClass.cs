4cf94a022fc02797c933e05e4ad9678e6d4f095c
Mikhail Gerasimov <mgerasim@inbox.ru>
2019-05-19 10:39:19 +1000

    Обновление проекта

---
+++ IndexMobile/Common/BaseClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Entity.Common
+{
+    public class BaseClass<T>
+    {
+        public virtual int ID { get; set; }
+        public virtual DateTime created_at { get; set; }
+        public virtual DateTime updated_at { get; set; }
+
+        public BaseClass()
+        {
+            this.ID = -1;
+        }
+        public virtual void Save()
+        {
+            this.created_at = DateTime.Now;
+            this.updated_at = DateTime.Now;
+            BaseRepository<T> repo = new BaseRepository<T>();
+
+            repo.Save(this);
+        }
+
+        public virtual void Delete()
+        {
+            BaseRepository<T> repo = new BaseRepository<T>();
+
+            repo.Delete(this);
+        }
+
+        public virtual void Update()
+        {
+            this.updated_at = DateTime.Now;
+            BaseRepository<T> repo = new BaseRepository<T>();
+            repo.Update(this);
+        }
+
+        public static List<T> GetAll()
+        {
+            BaseRepository<T> repo = new BaseRepository<T>();
+            return (List<T>)repo.GetAll();
+        }
+
+        public static T GetByID(int ID)
+        {
+            BaseRepository<T> repo = new BaseRepository<T>();
+            return repo.GetById(ID);
+        }
+
+
+
+    }
+}
---
+++ IndexMobile/Common/BaseRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity.Common;
+using NHibernate;
+using NHibernate.Criterion;
+
+namespace Entity.Common
+{
+    public class BaseRepository<T>
+    {
+        #region IRepository<T> Members
+
+        internal void Save(BaseClass<T> entity)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Save(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        internal void Update(BaseClass<T> entity)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Update(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        internal void Delete(BaseClass<T> entity)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Delete(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        internal T GetById(int ID)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                return session.CreateCriteria(typeof(T)).Add(Restrictions.Eq("ID", ID)).UniqueResult<T>();
+
+            }
+        }
+
+        internal List<T> GetAll()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                ICriteria criteria = session.CreateCriteria(typeof(T));
+                criteria.AddOrder(Order.Asc("ID"));
+                return criteria.List<T>().ToList<T>();
+            }
+        }
+
+        #endregion
+
+
+    }
+}
